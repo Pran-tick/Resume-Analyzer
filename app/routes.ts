f22b40a4ae51dcc638a805af041da85b57970606
Prantik Verma <prantikkvermaa4@gmail.com>
2025-08-06 23:38:50 +0530

    changed upload route name

--- app/routes.ts
+++ app/routes.ts
@@ -4,7 +4,7 @@ import auth from "./routes/auth";
 export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/auth.tsx'),
-    route('/upload', 'routes/upload.tsx'),
+    route('/Upload', 'routes/Upload.tsx'),
     route('/resume/:id', 'routes/resume.tsx'),
     route('/wipe', 'routes/wipe.tsx'),
 ] satisfies RouteConfig;
